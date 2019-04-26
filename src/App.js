156b15918b0e3e141fef15717b39b494748c15fd
hiroaki.sasaki <hiroaki.sasaki@hiroakisasakinoMacBook-Pro.local>
2019-04-26 12:51:28 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    // valueを初期化
+    this.state = {
+      todoList: [],
+      value: ''
+    };
+  }
+
+  onChange(e) {
+    // e.target.valueでinputのvalueを取得
+    this.setState({value: e.target.value})
+  }
+
+  add() {
+    this.setState({
+      todoList: this.state.todoList.concat(this.state.value),
+      value: '',
+    })
+    console.log(this.state.value);
+  }
+
+  render() {
+    // todoList一覧
+    const todoListNode = this.state.todoList.map((todo, idx) => {
+      return <li key={idx}>{todo}</li>
+    })
+
+    return (
+      <div>
+        <h1>TODO</h1>
+        <div>
+          <input type="text"
+            value={this.state.value}
+            onChange={e => this.onChange(e)}
+          />
+          <p>{this.state.value}</p>
+        </div>
+        <button onClick={() => this.add()}>追加</button>
+        <ul>
+          <p>{todoListNode}</p>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
